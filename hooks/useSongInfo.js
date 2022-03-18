46b0065908a8df5ef572c290fbfb4f45b6513a39
Dio Syahrizal <diosyahrizal@gmail.com>
2022-03-18 15:09:14 +0700

    feat: add logic & design player

--- hooks/useSongInfo.js
+++ hooks/useSongInfo.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useRecoilState } from "recoil";
 import { currentTrackIdState } from "../atoms/songAtom";
 import useSpotify from "./useSpotify";
--- pages/login.js
+++ pages/login.js
@@ -4,8 +4,6 @@ import { getProviders, signIn } from "next-auth/react";
 function Login({ providers }) {
   return (
     <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
-      {console.log(process.env.NEXT_PUBLIC_LOGIN_URL)}
-
       <img className="w-52 mb-5" src="https://links.papareact.com/9xl" alt="" />
       {Object.values(providers).map((provider, key) => (
         <div key={key}>
